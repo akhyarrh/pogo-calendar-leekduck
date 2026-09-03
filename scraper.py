05e61c1370c728276dcf9bcaaa775afbda80d726
akhyarrh <akhyarrh@gmail.com>
2026-09-03 17:06:50 +0800

    Update timezone in scraper.py to Asia/Singapore

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ class LeekDuckScraper:
 
     def __init__(self):
         self.url = "https://leekduck.com/events/"
-        self.timezone = pytz.timezone('Europe/Brussels')
+        self.timezone = pytz.timezone('Asia/Singapore')
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'
         }
